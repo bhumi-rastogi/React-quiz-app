18effbc50fbafc0446060738593858930c28de82
bhumi-rastogi <bhumi.rastogi05@gmail.com>
2023-12-08 01:28:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./App.css";
+import QuestionsWidget from "./components/QuestionsWidget.jsx";
+
+function App() {
+  return (
+    <QuestionsWidget/>
+  );
+}
+
+export default App;
